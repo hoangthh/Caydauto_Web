7018373c9f02892b0e4ed68ab8c4e2096bf8e71e
Jiro <huymachgia555@gmail.com>
2025-03-26 09:10:31 +0700

    Minor Changes

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddf111dab1bc9f37f20163df5419f2a1ea406522")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97f37dd7ed6d2524a0e7138aab0d4fa07ba9785a")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
