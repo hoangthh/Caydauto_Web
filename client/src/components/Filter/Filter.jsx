e75fec4a5a147346fc3194a9d7b455492a2bbe83
Tran Huy Hoang <tranhoang202204@gmail.com>
2025-03-31 00:30:54 +0700

    Custom product page detail page

--- client/src/components/Filter/Filter.jsx
+++ client/src/components/Filter/Filter.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Filter.scss";
 import { styled, Typography } from "@mui/material";
+import { fetchFilter } from "../../apis/filter";
 
 const FilterHeader = styled(Typography)`
   font-size: 25px;
@@ -16,6 +17,22 @@ const FilterSubHeader = styled(Typography)`
 const FilterItem = styled(Typography)``;
 
 export const Filter = () => {
+  const [categoryList, setCategoryList] = useState([]);
+  const [brandList, setBrandList] = useState([]);
+  const [priceList, setPriceList] = useState([]);
+  const [colorList, setColorList] = useState([]);
+
+  useEffect(() => {
+    const fetchFilterList = async () => {
+      const filter = await fetchFilter();
+      setCategoryList(filter.categories);
+      setBrandList(filter.brands);
+      setPriceList(filter.prices);
+      setColorList(filter.colors);
+    };
+    fetchFilterList();
+  }, []);
+
   return (
     <div className="filter">
       <FilterHeader>Lọc sản phẩm</FilterHeader>
@@ -24,70 +41,46 @@ export const Filter = () => {
 
       <div className="filter--category">
         {/* Category Item */}
-        <div className="filter--category__item">
-          <FilterItem>Tất cả sản phẩm</FilterItem>
-          <input type="checkbox" className="input" />
-        </div>
+        {categoryList.map((category) => (
+          <div className="filter--category__item" key={category.id}>
+            <FilterItem>{category.name}</FilterItem>
+            <input type="checkbox" className="input" />
+          </div>
+        ))}
         {/* Category Item */}
-        <FilterItem>Hộp bút - Túi bút</FilterItem>
-        <FilterItem>Vở học sinh</FilterItem>
-        <FilterItem>Bút chì</FilterItem>
-        <FilterItem>Bút gel</FilterItem>
-        <FilterItem>Bút ghi nhớ</FilterItem>
-        <FilterItem>Băng keo</FilterItem>
-        <FilterItem>Dụng cụ hỗ trợ</FilterItem>
       </div>
 
       <FilterSubHeader>Thương hiệu</FilterSubHeader>
 
       <div className="filter--brands">
-        <div className="filter--brands__item">
-          <FilterItem>Crayonia</FilterItem>
-          <input type="checkbox" className="input" />
-        </div>
-        <div className="filter--brands__item">
-          <FilterItem>Pilot</FilterItem>
-          <input type="checkbox" className="input" />
-        </div>
-        <div className="filter--brands__item">
-          <FilterItem>Marvy</FilterItem>
-          <input type="checkbox" className="input" />
-        </div>
-        <div className="filter--brands__item">
-          <FilterItem>Stabilo</FilterItem>
-          <input type="checkbox" className="input" />
-        </div>
+        {brandList.map((brand, index) => (
+          <div className="filter--brands__item" key={index}>
+            <FilterItem>{brand}</FilterItem>
+            <input type="checkbox" className="input" />
+          </div>
+        ))}
       </div>
 
       <FilterSubHeader>Giá</FilterSubHeader>
 
       <div className="filter--price">
-        <div className="filter--price__item">
-          <FilterItem>Dưới 100.000</FilterItem>
-          <input type="checkbox" className="input" />
-        </div>
-        <div className="filter--price__item">
-          <FilterItem>100.000 - 500.000</FilterItem>
-          <input type="checkbox" className="input" />
-        </div>
-        <div className="filter--price__item">
-          <FilterItem>Trên 500.000</FilterItem>
-          <input type="checkbox" className="input" />
-        </div>
+        {priceList.map((price, index) => (
+          <div className="filter--price__item" key={index}>
+            <FilterItem>{price.label}</FilterItem>
+            <input type="checkbox" className="input" />
+          </div>
+        ))}
       </div>
 
       <FilterSubHeader>Màu</FilterSubHeader>
       <div className="filter--color">
-        <input type="checkbox" className="input color white" />
-        <input type="checkbox" className="input color gray" />
-        <input type="checkbox" className="input color yellow" />
-        <input type="checkbox" className="input color orange" />
-        <input type="checkbox" className="input color black" />
-        <input type="checkbox" className="input color red" />
-        <input type="checkbox" className="input color blue" />
-        <input type="checkbox" className="input color pink" />
-        <input type="checkbox" className="input color green" />
-        <input type="checkbox" className="input color brown" />
+        {colorList.map((color) => (
+          <input
+            key={color.id}
+            type="checkbox"
+            className={`input filter-color ${color.name}`}
+          />
+        ))}
       </div>
     </div>
   );
