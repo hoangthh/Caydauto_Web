22b0442269e30c27fa772a497bf9aac9e6ea79c5
Tran Huy Hoang <tranhoang202204@gmail.com>
2025-04-02 07:45:30 +0700

    Custom cart product cart page

--- client/src/components/Filter/Filter.jsx
+++ client/src/components/Filter/Filter.jsx
@@ -22,6 +22,15 @@ export const Filter = () => {
   const [priceList, setPriceList] = useState([]);
   const [colorList, setColorList] = useState([]);
 
+  const [filters, setFilters] = useState({
+    categories: [],
+    brands: [],
+    colors: [],
+    prices: [],
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
   useEffect(() => {
     const fetchFilterList = async () => {
       const filter = await fetchFilter();
@@ -33,6 +42,44 @@ export const Filter = () => {
     fetchFilterList();
   }, []);
 
+  const handleChange = (event, filterValue, flag) => {
+    setFilters((prevFilters) => {
+      if (flag === "price") {
+        let newPrices = [...prevFilters.prices];
+        let newMinPrices = prevFilters.minPrice ? [prevFilters.minPrice] : [];
+        let newMaxPrices = prevFilters.maxPrice ? [prevFilters.maxPrice] : [];
+
+        if (newPrices.includes(filterValue.label)) {
+          newPrices = newPrices.filter((label) => label !== filterValue.label);
+          newMinPrices = newMinPrices.filter((min) => min !== filterValue.min);
+          newMaxPrices = newMaxPrices.filter((max) => max !== filterValue.max);
+        } else {
+          newPrices.push(filterValue.label);
+          newMinPrices.push(filterValue.min);
+          newMaxPrices.push(filterValue.max);
+        }
+
+        return {
+          ...prevFilters,
+          prices: newPrices,
+          minPrice: newMinPrices.length ? Math.min(...newMinPrices) : null,
+          maxPrice: newMaxPrices.length ? Math.max(...newMaxPrices) : null,
+        };
+      }
+
+      const filterItems = prevFilters[event.target.name];
+      const isSelected = filterItems.includes(filterValue);
+
+      return {
+        ...prevFilters,
+        [event.target.name]: isSelected
+          ? filterItems.filter((filter) => filter !== filterValue)
+          : [...filterItems, filterValue],
+      };
+    });
+  };
+
+  console.log(filters);
   return (
     <div className="filter">
       <FilterHeader>Lọc sản phẩm</FilterHeader>
@@ -44,7 +91,14 @@ export const Filter = () => {
         {categoryList.map((category) => (
           <div className="filter--category__item" key={category.id}>
             <FilterItem>{category.name}</FilterItem>
-            <input type="checkbox" className="input" />
+            <input
+              type="checkbox"
+              className="input"
+              name="categories"
+              checked={filters.categories.includes(category.id)}
+              // onChange={() => handleCategoryChange(category.id)}
+              onChange={(event) => handleChange(event, category.id)}
+            />
           </div>
         ))}
         {/* Category Item */}
@@ -56,7 +110,13 @@ export const Filter = () => {
         {brandList.map((brand, index) => (
           <div className="filter--brands__item" key={index}>
             <FilterItem>{brand}</FilterItem>
-            <input type="checkbox" className="input" />
+            <input
+              type="checkbox"
+              className="input"
+              name="brands"
+              checked={filters.brands.includes(brand)}
+              onChange={(event) => handleChange(event, brand)}
+            />
           </div>
         ))}
       </div>
@@ -67,7 +127,12 @@ export const Filter = () => {
         {priceList.map((price, index) => (
           <div className="filter--price__item" key={index}>
             <FilterItem>{price.label}</FilterItem>
-            <input type="checkbox" className="input" />
+            <input
+              type="checkbox"
+              className="input"
+              name="prices"
+              onChange={(event) => handleChange(event, price, "price")}
+            />
           </div>
         ))}
       </div>
@@ -79,6 +144,9 @@ export const Filter = () => {
             key={color.id}
             type="checkbox"
             className={`input filter-color ${color.name}`}
+            name="colors"
+            checked={filters.colors.includes(color.id)}
+            onChange={(event) => handleChange(event, color.id)}
           />
         ))}
       </div>
